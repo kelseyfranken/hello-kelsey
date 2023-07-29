a45eb99b35bab07b9777fb733e473d16b834aa30
Kelsey Franken <kcfranken@uc.cl>
2023-07-29 13:08:48 -0400

    :construction: add fade in effect

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
+import FadeIn from "react-fade-in/lib/FadeIn";
 
 function Sidebar(props) {
   const sections = [
@@ -12,36 +13,38 @@ function Sidebar(props) {
   ];
   return (
     <nav className="text-aqua flex flex-col gap-3 p-4 pb-32 pr-32 sticky bottom-0 left-full w-fit  max-sm:hidden">
-      <div className="flex flex-col">
-        {sections.map((section, index) => (
+      <FadeIn delay={120}>
+        <div className="flex flex-col">
+          {sections.map((section, index) => (
+            <a
+              href={section.url}
+              key={index}
+              className="font-bold text-l leading-normal hover:text-lylla"
+            >
+              {index}. {section.title}
+            </a>
+          ))}
+        </div>
+        <div className="flex gap-2">
           <a
-            href={section.url}
-            key={index}
-            className="font-bold text-l leading-normal hover:text-lylla"
+            href="https://github.com/kelseyfranken"
+            target="_blank"
+            className="hover:text-lylla"
           >
-            {index}. {section.title}
+            <GitHubIcon />
           </a>
-        ))}
-      </div>
-      <div className="flex gap-2">
-        <a
-          href="https://github.com/kelseyfranken"
-          target="_blank"
-          className="hover:text-lylla"
-        >
-          <GitHubIcon />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/kelseyfranken/"
-          target="_blank"
-          className="hover:text-lylla"
-        >
-          <LinkedInIcon />
-        </a>
-        <a href="mailto:kfrankenb@gmail.com" className="hover:text-lylla">
-          <EmailIcon />
-        </a>
-      </div>
+          <a
+            href="https://www.linkedin.com/in/kelseyfranken/"
+            target="_blank"
+            className="hover:text-lylla"
+          >
+            <LinkedInIcon />
+          </a>
+          <a href="mailto:kfrankenb@gmail.com" className="hover:text-lylla">
+            <EmailIcon />
+          </a>
+        </div>
+      </FadeIn>
     </nav>
   );
 }

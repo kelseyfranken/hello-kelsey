768b99cb827da6e90290b1d7d7660db565e439bb
Kelsey Franken <kcfranken@uc.cl>
2023-08-13 17:59:39 -0400

    :construction: add about section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,15 @@
+import About from "@/components/About";
 import Landing from "@/components/Landing";
 
 export default function Home() {
   return (
-    <>
+    <div className="sm:pl-[10%]">
       <section className="h-screen" id="home">
         <Landing />
       </section>
-      <section className="h-screen bg-lylla" id="about">
-        About
+      <section className="h-screen" id="about">
+        <About />
       </section>
-    </>
+    </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import FadeIn from "react-fade-in/lib/FadeIn";
+import SectionTitle from "./SectionTitle";
+
+export default function About() {
+  return (
+    <div className="pt-28 sm:pt-80">
+      <SectionTitle name={"About"} number={"1"} />
+      About
+    </div>
+  );
+}

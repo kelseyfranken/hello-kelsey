93f6ba7bd610c50c1f86e46f01349451117cf14c
Kelsey Franken <kcfranken@uc.cl>
2023-09-10 19:24:09 -0300

    :sparkles: add minimal cat animation

---
+++ src/components/MinimalCat.jsx
@@ -0,0 +1,18 @@
+import "./styles/minimalCat.css";
+export default function MinimalCat() {
+  return (
+    <div className="relative h-[170px] w-[192.1px]">
+      <div className="ear ear--left"></div>
+      <div className="ear ear--right"></div>
+      <div className="face">
+        <div className="eye eye--left">
+          <div className="eye-pupil"></div>
+        </div>
+        <div className="eye eye--right">
+          <div className="eye-pupil"></div>
+        </div>
+        <div className="muzzle"></div>
+      </div>
+    </div>
+  );
+}
